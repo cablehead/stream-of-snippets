5b1a4e5db3c60808b5d6ad1d049bdb09ee2d02dd
Andy Gayton <andy@thecablelounge.com>
2024-11-11 02:06:20 -0500

    default prefersDark to the user's system preference

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,10 +1,20 @@
 import { createEffect, createSignal } from "solid-js";
 
+import { createPrefersDark } from "@solid-primitives/media";
+
 import githubTheme from "highlight.js/styles/github.css?raw";
 import nordTheme from "highlight.js/styles/nord.css?raw";
 
-const [prefersDark, setPrefersDark] = createSignal(false);
+const systemPrefersDark = createPrefersDark();
+
+const [prefersDark, setPrefersDark] = createSignal(systemPrefersDark());
 const toggleTheme = () => setPrefersDark(!prefersDark());
+
+createEffect(() => {
+  setPrefersDark(systemPrefersDark());
+});
+
+export { prefersDark, toggleTheme };
 
 const applyTheme = (css: string) => {
   let styleTag = document.getElementById("dynamic-theme-style");
@@ -20,5 +30,3 @@ createEffect(() => {
   document.body.dataset.theme = prefersDark() ? "dark" : "light";
   applyTheme(prefersDark() ? nordTheme : githubTheme);
 });
-
-export { prefersDark, toggleTheme };
